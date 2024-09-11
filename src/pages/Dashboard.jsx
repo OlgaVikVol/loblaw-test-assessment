86e7bb07ba5f5d9f9120311d0c035bdd48768395
Olga Volobueva <olvik1605@gmail.com>
2024-09-11 11:43:54 -0400

    init commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+const DashBoard = () => {
+	return (
+		<div>
+      <h1>Campaign Dashboard for </h1>
+      <div>Current Number: </div>
+      <div>Total Impressions: </div>
+      <div>Total Clicks: </div>
+      <div>CTR: %</div>
+      <div>Total Users: </div>
+    </div>
+	)
+}
+
+export default DashBoard;
